2697418f805ded3d981f79195109607da49ec401
dilcom <dilcom3107@gmail.com>
2015-05-23 18:57:30 +0300

    spec fix: require gem after codeclimate reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,15 @@
 require 'simplecov'
 require 'codeclimate-test-reporter'
-require 'pilot-gnuplot'
 require 'digest'
 require 'chunky_png'
 require 'digest/md5'
-
 SimpleCov.add_filter 'vendor'
 SimpleCov.add_filter 'samples'
 SimpleCov.formatter = CodeClimate::TestReporter::Formatter
 SimpleCov.start CodeClimate::TestReporter.configuration.profile
+
+require 'pilot-gnuplot'
+
 
 include ChunkyPNG::Color
 
