969cac3dbead0e656ffff3ca1406fe260b274aed
dilcom <dilcom3107@gmail.com>
2015-05-25 12:07:56 +0300

    change updatable plot example due to gem changes

--- samples/updatable_plots/plot.rb
+++ samples/updatable_plots/plot.rb
@@ -5,11 +5,15 @@ x = (0..500).map{ |xx| xx/1000.0 }
 y = x.map{ |xx| Math.exp(-xx) }
 datablock = Datablock.new([x, y], true) # < this variant uses temporary file to store and update data
 # datablock = Datablock.new([x, y]) #< this variant resends whole datablock to pipe each time it is updated
-Plot.new([datablock, with: 'lines', title: 'Exp (points)'], term: ['qt', persist: true]).plot
-
+plot = Plot.new([datablock, with: 'lines', title: 'Exp (points)'], term: ['qt', persist: true])
+plot.plot
 puts 'Hit return to continue'
 gets
 
 x = (500..1000).map{ |xx| xx/1000.0 }
 y = x.map{ |xx| Math.exp(-xx) }
-datablock.update([x, y])
+#datablock.update!([x, y])
+#plot.plot
+
+plot2 = plot.update_data(datablock, datablock.update([x, y]))
+plot2.plot
