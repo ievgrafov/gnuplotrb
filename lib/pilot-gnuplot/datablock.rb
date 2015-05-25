bb09487630f0e70f2a33ce932db77861cfeab85c
dilcom <dilcom3107@gmail.com>
2015-05-25 10:03:00 +0300

    make datablock copy-on-update by default (still have forced version via #update!)

--- lib/pilot-gnuplot/datablock.rb
+++ lib/pilot-gnuplot/datablock.rb
@@ -10,48 +10,63 @@ module Gnuplot
     # ==== Parameters
     # * *data* - anything with +#to_points+ method
     # * *is_file* set to true will force this datablock to store its data in temporary file
-    def initialize(data, is_file = false)
+    # * *options* are for inner use (to create new Datablock from existing one)
+    def initialize(data, is_file = false, **options)
       @is_file = is_file
-      data = data.to_points
-      if @is_file
+      if options[:copy_file]
         name = Dir::Tmpname.make_tmpname('tmp_data', 0)
-        File.write(name, data)
+        FileUtils.cp(options[:old_file_name], name)
         @name = name
         ObjectSpace.define_finalizer(self, proc { File.delete(name) })
+        update!(data)
       else
-        @data = data
+        data = data.to_points
+        if @is_file
+          name = Dir::Tmpname.make_tmpname('tmp_data', 0)
+          File.write(name, data)
+          @name = name
+          ObjectSpace.define_finalizer(self, proc { File.delete(name) })
+        else
+          @data = data
+        end
       end
       yield(self) if block_given?
     end
 
     ##
     # ==== Overview
-    # This will add points to datablock
+    # Add points to existing datablock
     # ==== Parameters
     # * *data* - anything with +#to_points+ method
-    # * *update* - set to true (by default) will try to call replot on last used terminal
-    def update(data, update = true)
+    def update!(data)
       if @is_file
         File.open(@name, 'a') { |f| f.puts "\n#{data.to_points}"}
       else
         @data += data.to_points
-        @last_terminal.store_datablock(@name, @data) if update
       end
-      @last_terminal.replot if update
+    end
+
+    ##
+    # ==== Overview
+    # Instantiate one more Datablock with updated data
+    # ==== Parameters
+    # * *data* - anything with +#to_points+ method
+    def update(data)
+      if @is_file
+        Datablock.new(data, true, copy_file: true, old_file_name: @name)
+      else
+        Datablock.new(@data + data.to_points, false)
+      end
     end
 
     ##
     # ==== Overview
     # Returns quoted filename if datablock stored in file and outputs
     # datablock to gnuplot if not
-    # *gnuplot_term* should be given if datablock changes gnuplot instance
+    # *gnuplot_term* should be given if datablock not saved in file
     def name(gnuplot_term = nil)
-      @last_terminal ||= gnuplot_term
-      if @is_file
-        "'#{@name}'"
-      else
-        @name = @last_terminal.store_datablock(@data) unless @is_file
-      end
+      raise ArgumentError.new('No terminal given to output datablock') unless @is_file unless gnuplot_term
+      @is_file ? "'#{@name}'" : gnuplot_term.store_datablock(@data)
     end
 
     alias_method :to_s, :name
