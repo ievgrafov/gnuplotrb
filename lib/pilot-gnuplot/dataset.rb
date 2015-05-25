47d9451aba082fd924509c89e76b05867306c045
dilcom <dilcom3107@gmail.com>
2015-05-25 11:12:00 +0300

    teach dataset to update datablocks

--- lib/pilot-gnuplot/dataset.rb
+++ lib/pilot-gnuplot/dataset.rb
@@ -28,13 +28,13 @@ module Gnuplot
     # ==== Parameters
     # * *terminal* - must be given if Datablock does not use temp file
     def to_s(term = nil)
-      "#{@not_datablock ? @data : @data.name(term)} #{options}"
+      "#{@not_datablock ? @data : @data.name(term)} #{options_to_string}"
     end
 
     ##
     # ==== Overview
     # Create string from own options
-    def options
+    def options_to_string
       # order is significant for some options
       order = %w{index using axes title}
       @options = @options.sort_by{ |key, _| order.find_index(key.to_s) || 999}
@@ -46,6 +46,27 @@ module Gnuplot
           "#{key} #{value}"
         end
       end.join(' ')
+    end
+
+    ##
+    # ==== Overview
+    # Check if this Dataset uses *old_one* datablock. Create new Dataset with replaces datablock if it uses and return self otherwise.
+    # ==== Parameters
+    # * *old_one* - datablock object that should be replaced
+    # * *new_one* - replacement for old_one
+    def update_datablock(old_one, new_one)
+      (@not_datablock || old_one != @data) ? self : Dataset.new(new_one, @options)
+    end
+
+    ##
+    # ==== Overview
+    # Check if this Dataset uses *old_one* datablock. Replaces old datablock with the new one.
+    # ==== Parameters
+    # * *old_one* - datablock object that should be replaced
+    # * *new_one* - replacement for old_one
+    def update_datablock!(old_one, new_one)
+      @data = new_one unless @not_datablock if old_one === @data
+      self
     end
   end
 end
