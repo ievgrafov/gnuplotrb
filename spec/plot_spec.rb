4b37637002a81674246d04b04fbc1517c9d0fc86
dilcom <dilcom3107@gmail.com>
2015-05-26 11:55:00 +0300

    add specs to test Plot creation

--- spec/plot_spec.rb
+++ spec/plot_spec.rb
@@ -1,6 +1,29 @@
 require 'spec_helper.rb'
 
 describe Plot do
+  context 'creation' do
+    before do
+      @title = 'Awesome spec'
+      @formula =  %w(sin(x) cos(x) exp(-x))
+      @options = {title: @title, term: 'dumb'}
+    end
+
+    it 'should be created out of sequence of datasets' do
+      datasets = @formula.map { |formula| Dataset.new(formula) }
+      expect(Plot.new(*datasets)).to be_an_instance_of(Plot)
+    end
+
+    it 'should be created out of sequence of arrays' do
+      expect(Plot.new(*@formula)).to be_an_instance_of(Plot)
+    end
+
+    it 'should set options passed to constructor' do
+      plot = Plot.new(*@formula, **@options)
+      expect(plot).to be_an_instance_of(Plot)
+      expect(plot.title).to eql(@title)
+    end
+  end
+
   context 'options handling' do
     before do
       @options = {title: 'Gnuplot::Plot', yrange: 0..3}
