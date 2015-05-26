243ed0c1f6b00b6deeaa32b2f8b66597fa4b386f
dilcom <dilcom3107@gmail.com>
2015-05-26 11:08:00 +0300

    update examples

--- samples/updatable_plots/plot.rb
+++ samples/updatable_plots/plot.rb
@@ -1,19 +1,27 @@
 require 'pilot-gnuplot'
 include Gnuplot
 
-x = (0..500).map{ |xx| xx/1000.0 }
-y = x.map{ |xx| Math.exp(-xx) }
-datablock = Datablock.new([x, y], true) # < this variant uses temporary file to store and update data
-# datablock = Datablock.new([x, y]) #< this variant resends whole datablock to pipe each time it is updated
-plot = Plot.new([datablock, with: 'lines', title: 'Exp (points)'], term: ['qt', persist: true])
+x = (0..500).map { |xx| xx / 1000.0 }
+y = x.map { |xx| Math.exp(-xx) }
+
+plot = Plot.new([[x, y], with: 'lines', title: 'Exp (points)'],
+                term: ['qt', persist: true])
 plot.plot
 puts 'Hit return to continue'
 gets
 
-x = (500..1000).map{ |xx| xx/1000.0 }
-y = x.map{ |xx| Math.exp(-xx) }
-#datablock.update!([x, y])
-#plot.plot
+x = (500..1000).map { |xx| xx / 1000.0 }
+y = x.map { |xx| Math.exp(-xx) }
+# datablock.update!([x, y])
+# plot.plot
 
-plot2 = plot.update_datablock(datablock, datablock.update([x, y]))
-plot2.plot
+plot2 = plot.update_dataset([x, y])
+plot3 = plot.add_dataset(['sin(x)', title: 'Sin(x)'])
+
+plot2.plot(plot.terminal)
+
+plot3.plot
+puts 'Hit return to continue'
+gets
+
+plot.plot
