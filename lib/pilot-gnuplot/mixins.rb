1f8e516d66ceee4953065be05f6e7a8862dd1aaa
dilcom <dilcom3107@gmail.com>
2015-05-26 08:01:00 +0300

    patch String with #to_points

--- lib/pilot-gnuplot/mixins.rb
+++ lib/pilot-gnuplot/mixins.rb
@@ -3,12 +3,18 @@ class Array
   def to_points
     return '' if self.empty?
     case self[0]
-      when Array
-        self[0].zip( *self[1..-1] ).map { |a| a.join(' ') }.join("\n")
-      when Numeric
-        self.join("\n")
-      else
-        self[0].zip( *self[1..-1] ).to_points
+    when Array
+      self[0].zip(*self[1..-1]).map { |a| a.join(' ') }.join("\n")
+    when Numeric
+      join("\n")
+    else
+      self[0].zip(*self[1..-1]).to_points
     end
+  end
+end
+
+class String
+  def to_points
+    return self.clone
   end
 end
