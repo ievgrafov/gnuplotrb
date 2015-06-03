34d7099f4d4386a8d5d89ca191ad0353155ff53a
dilcom <dilcom3107@gmail.com>
2015-06-03 11:34:23 +0300

    add specs for Terminal

--- spec/terminal_spec.rb
+++ spec/terminal_spec.rb
@@ -1,8 +1,23 @@
 require 'spec_helper.rb'
 
 describe Terminal do
+  before(:all) do
+    @tmp_dir = File.join('spec', 'tmp')
+    Dir.mkdir(@tmp_dir)
+    @datafile_path = File.join('spec', 'points.data')
+  end
+
+  after(:all) do
+    FileUtils.rm_rf(@tmp_dir)
+  end
+
   before do
     @terminal = Terminal.new
+    @paths = (0..1).map { |i| File.join(@tmp_dir, "#{i}plot.png") }
+    @options0 = { term: ['png', size: [300,300]], output: @paths[0] }
+    @options1 = { term: ['png', size: [300,300]], output: @paths[1] }
+    @dataset = Dataset.new('sin(x)')
+    @plot = Plot.new(@dataset, @options0)
   end
 
   context 'options handling' do
@@ -27,6 +42,46 @@ describe Terminal do
       options = {term: ['qt', size: [100, 100]]}
       string = @terminal.options_hash_to_string(options)
       expect(string.strip).to be_eql("set term qt size 100,100")
+    end
+  end
+
+  context '#<<' do
+    it 'should output Plots' do
+      @plot.plot
+      FileUtils.mv(@paths[0], @paths[1])
+      @terminal << @plot
+      expect(same_images?(*@paths)).to be_truthy
+    end
+
+    it 'should output Datasets' do
+      @plot.plot
+      @terminal.set(@options1)
+      @terminal << @dataset
+      expect(same_images?(*@paths)).to be_truthy
+    end
+
+    it 'should output whatever you want just as string' do
+      @plot.plot
+      @terminal << @terminal.options_hash_to_string(@options1)
+      @terminal << @dataset
+      expect(same_images?(*@paths)).to be_truthy
+    end
+  end
+
+  context '#puts' do
+    it 'should output whatever you want just as string' do
+      @plot.plot
+      @terminal.puts(@terminal.options_hash_to_string(@options1))
+      @terminal << @dataset
+      expect(same_images?(*@paths)).to be_truthy
+    end
+  end
+
+  context '#replot' do
+    it 'should replot last plotted graph on #replot call' do
+      @plot.plot(@terminal)
+      @terminal.replot(@options1)
+      expect(same_images?(*@paths)).to be_truthy
     end
   end
 end
