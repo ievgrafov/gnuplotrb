9c396c17e1d91c2dcbd234e46154ce9f21c53e75
dilcom <dilcom3107@gmail.com>
2015-06-09 12:33:33 +0300

    move options handling to module scope

--- lib/pilot-gnuplot.rb
+++ lib/pilot-gnuplot.rb
@@ -14,5 +14,33 @@ require 'pilot-gnuplot/mixins'
 # === Overview
 # Ruby bindings for gnuplot.
 module Gnuplot
+  # Some values of options should be quoted to be read by gnuplot
+  # TODO update list with data from gnuplot documentation !!!
+  QUOTED = %w(title output xlabel x2label ylabel y2label clabel cblabel zlabel rgb)
 
+  ##
+  # ==== Overview
+  # Recursive function that converts Ruby options to gnuplot
+  # ==== Parameters
+  # *option* - an option that should be converted
+  # ==== Examples
+  #   ['png', size: [300, 300]] => 'png size 300,300'
+  #   0..100 => '[0:100]'
+  def option_to_string(key = nil, option)
+    return key.to_s if !!option == option # check for boolean
+    value = case option
+            when Array
+              option.map { |el| option_to_string(el) }.join(option[0].is_a?(Numeric) ? ',' : ' ')
+            when Hash
+              option.map { |i_key, i_val| option_to_string(i_key, i_val) }.join(' ')
+            when Range
+              "[#{option.begin}:#{option.end}]"
+            else
+              option.to_s
+            end
+    value = "'#{value}'" if QUOTED.include?(key.to_s)
+    value = "#{key} " + value if key
+    value
+  end
 end
+
--- lib/pilot-gnuplot/terminal.rb
+++ lib/pilot-gnuplot/terminal.rb
@@ -1,8 +1,4 @@
 module Gnuplot
-  # Some values of options should be quoted to be read by gnuplot
-  # TODO update list with data from gnuplot documentation !!!
-  QUOTED = %w(title output xlabel x2label ylabel y2label clabel cblabel zlabel rgb)
-
   ##
   # === Overview
   # Terminal keeps open pipe to gnuplot process, cares about naming datablocks and
@@ -71,9 +67,10 @@ module Gnuplot
       #
       # Now checks only terminal name.
       def validate_options(options)
-        if options[:term]
-          term = options[:term].is_a?(Array) ? options[:term][0] : options[:term]
-          fail(ArgumentError, 'Seems like your Gnuplot does not support that terminal, please see supported terminals with Terminal#available_terminals') unless Terminal.valid_terminal?(term)
+        terminal = options[:term]
+        if terminal
+          terminal = terminal[0] if terminal.is_a?(Array)
+          fail(ArgumentError, 'Seems like your Gnuplot does not support that terminal, please see supported terminals with Terminal#available_terminals') unless valid_terminal?(terminal)
         end
       end
     end
@@ -107,28 +104,6 @@ module Gnuplot
 
     ##
     # ==== Overview
-    # Recursive function that converts Ruby options to gnuplot
-    # ==== Parameters
-    # *option* - an option that should be converted
-    # ==== Examples
-    #   ['png', size: [300, 300]] => 'png size 300,300'
-    #   0..100 => '[0:100]'
-    def option_to_string(option)
-      return '' if !!option == option # check for boolean
-      case option
-      when Array
-        option.map { |el| option_to_string(el) }.join(option[0].is_a?(Numeric) ? ',' : ' ')
-      when Hash
-        option.map { |pair| "#{pair[0]} #{option_to_string(pair[1])}" }.join(' ')
-      when Range
-        "[#{option.begin}:#{option.end}]"
-      else
-        option.to_s
-      end
-    end
-
-    ##
-    # ==== Overview
     # Converts given options to gnuplot format;
     # for {opt1: val1, .. , optN: valN} it return
     #   set opt1 val1
@@ -140,11 +115,7 @@ module Gnuplot
       result = ""
       options.each do |key, value|
         if value
-          if QUOTED.include?(key.to_s)
-            result += "set #{key} '#{option_to_string(value)}'\n"
-          else
-            result += "set #{key} #{option_to_string(value)}\n"
-          end
+          result += "set #{option_to_string(key, value)}\n"
         else
           result += "unset #{key}\n"
         end
