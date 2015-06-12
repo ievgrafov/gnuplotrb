73e592cf64e595e8cf26bb4ce98c53db32bc4186
dilcom <dilcom3107@gmail.com>
2015-06-12 16:15:14 +0300

    improve Plot docs and add small fixes

--- lib/pilot-gnuplot/plot.rb
+++ lib/pilot-gnuplot/plot.rb
@@ -3,12 +3,16 @@ module Gnuplot
   # === Overview
   # Plot correspond to simple 2D visualisation
   class Plot
+    ##
+    # Terminal object used by this Plot to pipe data to gnuplot.
     attr_reader :terminal
+    ##
+    # Array of datasets which are plotted by this object.
     attr_reader :datasets
     ##
-    # ==== Parameters
+    # ==== Arguments
     # * *datasets* are either instances of Dataset class or
-    #   [data, **dataset_options] arrays
+    #   [data, **dataset_options] arrays from which Dataset may be created
     # * *options* will be considered as 'settable' options of gnuplot
     #   ('set xrange [1:10]' for { xrange: 1..10 },
     #   "set title 'plot'" for { title: 'plot' } etc)
@@ -28,13 +32,12 @@ module Gnuplot
 
     ##
     # ==== Overview
-    # This outputs plot to term (if given) or to last used term (if any)
-    # or just builds its own Terminal with plot and options
-    # ==== Parameters
+    # This outputs plot to term (if given) or to this plot's own terminal.
+    # ==== Arguments
     # * *term* - Terminal to plot to
     # * *options* - will be considered as 'settable' options of gnuplot
-    # ('set xrange [1:10]', 'set title 'plot'' etc);
-    # options passed here have priority above already given to ::new
+    #   ('set xrange [1:10]', 'set title 'plot'' etc);
+    # Options passed here have priority over already existing.
     def plot(term = @terminal, **options)
       opts = @options.merge(options)
       full_command = @cmd + @datasets.map { |dataset| dataset.to_s(term) }.join(' , ')
@@ -44,14 +47,15 @@ module Gnuplot
     end
 
     ##
+    # Method for inner use.
     # ==== Overview
     # Method which outputs plot to specific terminal (possibly some file).
     # Explicit use should be avoided. This method is called from #method_missing
     # when it handles method names like #to_png(options).
-    # ==== Parameters
+    # ==== Arguments
     # * *terminal* - string corresponding to terminal type (png, html, jpeg etc)
     # * *path* - path to output file, if none given it will output to temp file
-    # and then read it and return binary data with contents of file
+    #   and then read it and return binary contents of file
     # * *options* - used in 'set term <term type> <options here>'
     # ==== Examples
     #   plot.to_png('./result.png', size: [300, 500])
@@ -80,22 +84,26 @@ module Gnuplot
     # A new object will be constructed with selected option set.
     # And finally you can get current value of any option using
     # #options_name without arguments.
+    # ===== Arguments
+    # * *option_value* - value to set an option. If none given
+    #   method will just return current option's value
     # ===== Examples
+    #   plot = Plot.new
     #   new_plot = plot.title('Awesome plot')
-    #   plot.title # >nil
-    #   new_plot.title # >'Awesome plot'
-    #   plot.title # >'One more awesome plot'
+    #   plot.title #=> nil
+    #   new_plot.title #=> 'Awesome plot'
     #
     # ==== Plotting to specific term
     # ===== Overview
     # Gnuplot offers possibility to output graphics to many image formats.
     # The easiest way to to so is to use #to_<plot_name> methods.
-    # ===== Parameters
+    # ===== Arguments
     # * *options* - set of options related to terminal (size, font etc).
+    #   Be careful, some terminals have their own specific options.
     # ===== Examples
-    #   # options specific for png term
+    #   # font options specific for png term
     #   plot.to_png('./result.png', size: [300, 500], font: ['arial', 12])
-    #   # options specific for svg term
+    #   # font options specific for svg term
     #   content = plot.to_svg(size: [100, 100], fname: 'Arial', fsize: 12)
     def method_missing(meth_id, *args)
       meth = meth_id.id2name
@@ -118,13 +126,13 @@ module Gnuplot
     # ==== Overview
     # Create new Plot object where dataset at *position* will
     # be replaced with the new one created from it by updating.
-    # ==== Parameters
+    # ==== Arguments
     # * *position* - position of dataset which you need to update
-    # (by default first dataset is updated)
+    #   (by default first dataset is updated)
     # * *data* - data to update dataset with
     # * *options* - options to update dataset with
     # ==== Example
-    #   TODO add examples (and specs!)
+    #   updated_plot = plot.update_dataset(data: [x1,y1], title: 'After update')
     def update_dataset(position = 0, data: nil, **options)
       old_ds = @datasets[position]
       new_ds = old_ds.update(data, options)
@@ -135,24 +143,30 @@ module Gnuplot
     # ==== Overview
     # Create new Plot object where dataset at *position* will
     # be replaced with the given one.
-    # ==== Parameters
+    # ==== Arguments
     # * *position* - position of dataset which you need to update
-    # (by default first dataset is replaced)
-    # * *dataset* - dataset to replace the old one
+    #   (by default first dataset is replaced)
+    # * *dataset* - dataset to replace the old one. You can also
+    #   give here [data, **dataset_options] array from
+    #   which Dataset may be created.
     # ==== Example
-    #   TODO add examples (and specs!)
+    #   sinx = Plot.new('sin(x)')
+    #   cosx = sinx.replace_dataset(['cos(x)'])
     def replace_dataset(position = 0, dataset)
-      self.class.new(@datasets.set(position, dataset), @options)
+      self.class.new(@datasets.set(position, convert_to_dataset(dataset)), @options)
     end
 
     ##
     # ==== Overview
     # Create new Plot object where given dataset will
     # be appended to dataset list.
-    # ==== Parameters
+    # ==== Arguments
     # * *dataset* - dataset to add
     # ==== Example
-    #   TODO add examples (and specs!)
+    #   sinx = Plot.new('sin(x)')
+    #   sinx_and_cosx = sinx.add(['cos(x)'])
+    #
+    #   cosx_and_sinx = sinx << ['cos(x)']
     def add_dataset(dataset)
       self.class.new(@datasets.add(convert_to_dataset(dataset)), @options)
     end
@@ -163,11 +177,13 @@ module Gnuplot
     # ==== Overview
     # Create new Plot object where given dataset will
     # be appended to dataset list.
-    # ==== Parameters
+    # ==== Arguments
     # * *position* - position of dataset that should be
-    # removed (by default last dataset is removed)
+    #   removed (by default last dataset is removed)
     # ==== Example
-    #   TODO add examples (and specs!)
+    #   sinx_and_cosx = Plot.new('sin(x)', 'cos(x)')
+    #   sinx = sinx_and_cosx.remove_dataset
+    #   cosx = sinx_and_cosx.remove_dataset(0)
     def remove_dataset(position = -1)
       self.class.new(@datasets.delete_at(position), @options)
     end
@@ -177,10 +193,15 @@ module Gnuplot
     # Replot self. Usable is cases then Plot contains
     # datasets which store data in files. Replot may be
     # used in this case to update plot after data update.
-    # # ==== Example
-    #   TODO add examples (and specs!)
-    def replot
-      @already_plotted ? plot_command(@terminal, 'replot', @options) : plot
+    # ==== Arguments
+    # * *options* - set of options related to terminal (size, font etc).
+    #   Be careful, some terminals have their own specific options.
+    # # ==== Examples
+    #   plot.replot(output: './result.png',
+    #               term: ['png', size: [300, 500], font: ['arial', 12]],
+    #               title: 'Replotted')
+    def replot(**options)
+      @already_plotted ? plot_command(@terminal, 'replot', @options.merge(options)) : plot(options)
     end
 
     ##
@@ -188,14 +209,15 @@ module Gnuplot
     # Create new Plot object where current Plot's
     # options are merged with given. If no options
     # given it will just return existing set of options.
-    # ==== Parameters
-    # * *options* - options to add
+    # ==== Arguments
+    # * *options* - options to add. If no options given, current
+    #   options are returned.
     # ==== Example
     #   sin_graph = Plot.new(['sin(x)', title: 'Sin'], title: 'Sin on [0:3]', xrange: 0..3)
     #   sin_graph.plot
     #   sin_graph_update = sin_graph.options(title: 'Sin on [-10:10]', xrange: -10..10)
     #   sin_graph_update.plot
-    #   # you may also consider this as
+    #   # this may also be considered as
     #   # sin_graph.title(...).xrange(...)
     def options(**options)
       if options.empty?
@@ -207,17 +229,22 @@ module Gnuplot
 
     ##
     # ==== Overview
-    # Get a dataset number *position*
+    # The same as Plot#datasets[args]
     def [](*args)
       @datasets[*args]
     end
 
+    ##
+    # Method for inner use.
+    # Check if given args is a dataset and returns it. Creates
+    # new dataset from given args otherwise.
     def convert_to_dataset(source)
       source.is_a?(Dataset) ? source.clone : Dataset.new(*source)
     end
 
     ##
-    # TODO: docs here
+    # Method for inner use.
+    # Outputs given command and options to terminal.
     def plot_command(term, full_command, options)
       output = options[:output]
       File.delete(output) if output && File.file?(output)
@@ -226,5 +253,9 @@ module Gnuplot
           .unset(options.keys)
       sleep 0.001 until File.file?(output) && File.size(output) > 100 if output
     end
+
+    private :convert_to_dataset,
+            :plot_command,
+            :to_specific_term
   end
 end
