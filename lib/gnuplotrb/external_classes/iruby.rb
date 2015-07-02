421d58efe3ca209e0b86bc2668c4dc2a42da4721
Ivan Evgrafov <dilcom@users.noreply.github.com>
2015-07-02 11:23:22 +0300

    replace tabs with spaces in iruby.rb

--- lib/gnuplotrb/external_classes/iruby.rb
+++ lib/gnuplotrb/external_classes/iruby.rb
@@ -1,16 +1,16 @@
 if defined?(IRuby)
   module GnuplotRB::Plottable
-  	def to_iruby
-  	  available_terminals = {
-  	  	'png'      => 'image/png',
-  	  	'pngcairo' => 'image/png',
-  	  	'jpeg'     => 'image/jpeg',
-  	  	'svg'      => 'image/svg',
-  	  	'dumb'     => 'text/plain'
-  	  }
-  	  terminal, options = term.is_a?(Array) ? [term[0], term[1]] : [term, {}]
-  	  terminal = 'svg' unless available_terminals.keys.include?(terminal)
-  	  [available_terminals[terminal], self.send("to_#{terminal}".to_sym, **options)]
-  	end
+    def to_iruby
+      available_terminals = {
+        'png'      => 'image/png',
+        'pngcairo' => 'image/png',
+        'jpeg'     => 'image/jpeg',
+        'svg'      => 'image/svg',
+        'dumb'     => 'text/plain'
+      }
+      terminal, options = term.is_a?(Array) ? [term[0], term[1]] : [term, {}]
+      terminal = 'svg' unless available_terminals.keys.include?(terminal)
+      [available_terminals[terminal], self.send("to_#{terminal}".to_sym, **options)]
+    end
   end
 end
