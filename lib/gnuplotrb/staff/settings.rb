62a71221cbda92640e348fb604ce4b443a22fd7e
dilcom <dilcom3107@gmail.com>
2015-08-09 14:03:20 +0300

    update docs of staff classes for YARD

--- lib/gnuplotrb/staff/settings.rb
+++ lib/gnuplotrb/staff/settings.rb
@@ -1,35 +1,37 @@
 module GnuplotRB
   ##
-  # This module takes care of path to gnuplot executable
-  # and checking its version.
+  # This module takes care of path to gnuplot executable and checking its version.
   module Settings
-    ##
-    # Since gem uses some modern gnuplot features it's
-    # required to have modern gnuplot installed.
     MIN_GNUPLOT_VERSION = 5.0
 
     class << self
       ##
-      # Max fit dely is used inside fit function.
-      # If it waits for output more than max_fit_delay seconds
-      # this behaviour is considered as errorneus.
       # For heavy calculations max_fit_delay may be increased.
       attr_writer :max_fit_delay
+      ##
+      # Get max fit delay.
+      #
+      # Max fit delay (5s by default) is used inside Fit::fit function.
+      # If it waits for output more than max_fit_delay seconds
+      # this behaviour is considered as errorneus.
+      # @return [Integer] seconds to wait for output
       def max_fit_delay
         @max_fit_delay ||= 5
       end
+
       ##
-      # ====== Overview
       # Get path that should be used to run gnuplot executable.
-      # Default value: 'gnuplot'
+      # Default value: 'gnuplot'.
+      # @return [String] path to gnuplot executable
       def gnuplot_path
         self.gnuplot_path = 'gnuplot' unless defined?(@gnuplot_path)
         @gnuplot_path
       end
 
       ##
-      # ====== Overview
       # Set path to gnuplot executable.
+      # @param path [String] path to gnuplot executable
+      # @return given path
       def gnuplot_path=(path)
         validate_version(path)
         opts = { stdin_data: "set term\n" }
@@ -41,29 +43,28 @@ module GnuplotRB
       end
 
       ##
-      # ====== Overview
-      # Get list of terminals (png, html, qt, jpeg etc)
-      # available for that gnuplot.
+      # Get list of terminals (png, html, qt, jpeg etc) available for this gnuplot.
+      # @return [Array of String] array of terminals available for this gnuplot
       def available_terminals
         gnuplot_path
         @available_terminals
       end
 
       ##
-      # ====== Overview
-      # Get gnuplot version. Uses gnuplot_path to find
-      # gnuplot executable.
+      # Get gnuplot version. Uses gnuplot_path to find gnuplot executable.
+      # @return [Numeric] gnuplot version
       def version
         gnuplot_path
         @version
       end
 
       ##
-      # ====== Overview
-      # Validates gnuplot version. Compares current gnuplot's
-      # version with ::MIN_GNUPLOT_VERSION.
-      # ====== Arguments
-      # * *path* - path to gnuplot executable.
+      # @private
+      # Validate gnuplot version. Compares current gnuplot's
+      # version with ::MIN_GNUPLOT_VERSION. Throws exception if version is
+      # less than min.
+      #
+      # @param path [String] path to gnuplot executable
       def validate_version(path)
         @version = IO.popen("#{path} --version")
                      .read
